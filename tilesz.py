ccccf688cc4808185b5c498a71f0bcead50e0c47
Vignesh <vbhavana@uwaterloo.ca>
2022-12-01 23:50:48 -0500

    Made class and 2 functions in tilesz.py.

--- tilesz.py
+++ tilesz.py
@@ -1,1 +1,10 @@
 import pygame
+
+# class made to create tiles in the screen
+class Tile (pygame.sprite.Sprite):
+    def __init__(self, pos, x_size, y_size, colour):
+        pass
+
+    # moves tiles along screen
+    def update(self, translate):
+         pass
