88706901ba44bc4355ff5d7e570cc3381f4b554b
Vignesh <vbhavana@uwaterloo.ca>
2022-12-02 14:43:28 -0500

    Finished level 1 and added screen constants.

---
+++ setting.py
@@ -0,0 +1,21 @@
+# maps objects displayed on screen
+level_map_1 = [
+'       XXX                                                         XXXXX XX                               XXXXXXX X         ',
+'XXX     X                             XX              XX           X     X             XXX X X                      XXXX    ',
+'XXX   X X                                                               X               X  X X              X               ',
+'XXX   XXX                                                              XX             X X   X     XXX  XXXXX     XXXX       ',
+'XXX                                XXXX    XX XXXX XX     XX  XX  XX   XX  XX         XXX   X                               ',
+'XXX                                                                    XX                             X X                   ',
+'XXXXXXXXXXXXXXXXXX               XX                                    XX     X XX  XXX         XXX                         ',
+'XXXXX                       XXXX                XXXX  XX  XX  XX  XX   XX  XX                    X    X    X                ',
+'XXXXX      XX     X                                                    XX                      X X   XXX  XXX  X X X X X F  ',
+'XXXXX  P   XX      X                                                   XX                   XX XXX    X    X                ',
+'XXXXXXXXXXXXX       X X  XXX   XX   X X  X  X  X   XX  X  XX  XX  XX   XX  XX          XXXXX                                ',]
+
+
+
+
+# tile and screen dimensions
+title_size = 64
+screen_width = 1200
+screen_height = len(level_map_1) * title_size
